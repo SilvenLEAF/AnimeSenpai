454f6226dd1d0ed0dbe1ad60e8ab6149cf2e9139
SilvenLEAF <SilvenLEAF@gmail.com>
2020-07-30 12:24:52 +0530

    basic database setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 
 // firing express app
@@ -8,6 +9,17 @@ const app = express();
 app.use(express.json()); //replacement of body-parser: gives us (req.body)
 
 
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}, (err)=>{
+  if(err) throw err;
+
+  console.log(`connected to MongoDB`);
+})
 
 /*  ----------------------------------
 .             middlewares 
